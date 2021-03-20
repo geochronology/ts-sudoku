8d47b95b0268e81e544946592f5dec48d97d7c0d
geochronology <54387644+geochronology@users.noreply.github.com>
2021-03-20 16:51:43 -0700

    bob

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from "styled-components";
+
+import { unregister } from './core';
+import { GlobalStyles, theme } from "./styles";
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <div>hello my name is bob</div>
+  </ThemeProvider>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+unregister();
+
