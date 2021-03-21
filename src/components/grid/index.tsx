174af350705449ab24cdbcc79edfcb3ed63de2f6
geochronology <54387644+geochronology@users.noreply.github.com>
2021-03-21 00:07:47 -0700

    completed grid styling

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,18 +1,19 @@
 import React, { FC, Children } from 'react'
 
+import Block from "./block"
+import { Container, Row } from "./styles";
+
 const Grid: FC = () => {
   return (
-    <div data-cy="grid-container">
+    <Container data-cy="grid-container">
       {Children.toArray([...Array(9)].map((_, rowIndex) => (
-        <div data-cy="grid-row-container" >
+        <Row data-cy="grid-row-container" >
           {Children.toArray([...Array(9)].map((_, colIndex) => (
-            <div data-cy="block">
-              {/* bl0k */}
-            </div>
+            <Block colIndex={colIndex} rowIndex={rowIndex} />
           )))}
-        </div>
+        </Row>
       )))}
-    </div>
+    </Container>
   )
 }
 
